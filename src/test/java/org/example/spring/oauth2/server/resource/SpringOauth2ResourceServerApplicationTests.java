7078738ed331c5a660ab71d4d40506a3ff0b4c05
Georgii Lvov <georgii.lvov@telekom.com>
2023-05-18 01:11:31 +0300

    Initial commit

---
+++ src/test/java/org/example/spring/oauth2/server/resource/SpringOauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spring.oauth2.server.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
